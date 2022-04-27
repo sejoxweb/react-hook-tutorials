40e2edfcaa50ed664c90a445db23bb5b000d4b0a
jafar1405 <jafar1405@gmail.com>
2022-04-27 08:36:55 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import "./App.css";
 import React, { Component } from "react";
-// import Card from "./components/Card";
-// import CardTwo from "./components/CardTwo";
-// import CardThree from "./components/CardThree";
-// import CardZero from "./components/CardZero";
 import ParentLifeCycle from "./components/ParentLifeCycle";
 import CounterHook from "./components/CounterHook";
 
